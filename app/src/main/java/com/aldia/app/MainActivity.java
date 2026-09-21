1e332df720ca2a3008f3ff7d1fbf87ed0ce3db48
helios21-hub <helios.hectorgonzalez4477@gmail.com>
2026-09-21 18:58:05 -0300

    Add files via upload

--- app/src/main/java/com/aldia/app/MainActivity.java
+++ app/src/main/java/com/aldia/app/MainActivity.java
@@ -297,7 +297,7 @@ public class MainActivity extends ComponentActivity {
             try {
                 return getPackageManager().getPackageInfo(getPackageName(), 0).versionName;
             } catch (Exception e) {
-                return "2.35";
+                return "2.36";
             }
         }
 
